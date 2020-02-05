2af5db82042f1c25eadc824be417b69198c7635e
Frek714 <46048535+Frek714@users.noreply.github.com>
2020-02-05 16:10:18 +0100

    inital commit back-end (creration of project)

---
+++ fishingachivment/src/main/java/it/federico/fishingachivment/FishingachivmentApplication.java
@@ -0,0 +1,13 @@
+package it.federico.fishingachivment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingachivmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingachivmentApplication.class, args);
+	}
+
+}
